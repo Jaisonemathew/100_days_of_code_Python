a29707d98d8eed523119b8a5be873bce027f51fa
Jaison E Mathew <jaisonemathew14@gmail.com>
2023-04-28 23:03:18 +0530

    Added day 14

---
+++ Day 14 higher-lower-completed/main.py
@@ -0,0 +1,28 @@
+import random
+from game_data import data
+score=0
+def run(guess,score,hold): 
+    if(guess==hold):
+      score=score+1
+      print(f"You are right current score is {score}")
+      main(score)
+    
+    else:
+      print(f"Sorry,that's wrong final score {score}")
+
+def main(score):
+  r1=random.randint(0,49)
+  r2=random.randint(0,49)
+  print(data[r1]['follower_count'])
+  print(f"Compare A:{data[r1]['name']},a {data[r1]['description']},from {data[r1]['country']} ")
+  print(data[r2]['follower_count'])
+  print(f"Compare A:{data[r2]['name']},a {data[r2]['description']},from {data[r2]['country']} ")
+  guess=input("Who has more followers A or B:")
+  def check(data):
+    if(data[r1]['follower_count']>data[r2]['follower_count']):
+      return 'A'
+    else:
+      return 'B'
+  hold=check(data)
+  run(guess,score,hold)
+main(score)
