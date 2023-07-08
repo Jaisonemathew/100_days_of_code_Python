1e220d773c847530253f408edb6a7ecfc0a7da3a
Jaison E Mathew <jaisonemathew14@gmail.com>
2023-07-08 15:26:56 +0530

    completed Day 22

--- Day 22 The Pong game/ball.py
+++ Day 22 The Pong game/ball.py
@@ -19,9 +19,9 @@ class Ball(Turtle):
         
     def bounce_x(self):
         self.x_move*=-1
-        self.move_speed*=0.9
-        
     def reset_position(self):
         self.goto(0,0)
-        self.move_speed=0.1
         self.bounce_x()
+        self.move_speed=0.1
+        
+    
