b7eeaae12b981863aee0381150c24f0f79809427
Jaison E Mathew <jaisonemathew14@gmail.com>
2023-07-08 22:18:21 +0530

    added Day 23

---
+++ Day 23 The turtle crossing project/scoreboard.py
@@ -0,0 +1,22 @@
+FONT = ("Courier", 24, "normal")
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+        self.goto(-280, 260)
+        self.update_scoreboard()
+        
+    def update_scoreboard(self):
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+    def increase_level(self):
+        self.level += 1
+        self.clear()
+        self.update_scoreboard()
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=FONT)
+   
