7230b168f1a7d62407f6cc39d7d6f9d7e0560ee8
Jaison E Mathew <jaisonemathew14@gmail.com>
2023-07-12 21:02:51 +0530

    added day 10 and 11 and 1

---
+++ Day 1/main.py
@@ -0,0 +1,14 @@
+print("Welcome to band name generator!")
+
+c=input("Enter the city where u grew up:")
+
+p=input("Enter the name of your pet:")
+
+print("Your band name is "+c+" "+p)
+
+
+
+
+
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://band-name-generator-end.appbrewery.repl.run/
