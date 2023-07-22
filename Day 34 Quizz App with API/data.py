c1e869bba43fef81aa869fe22f071668736b7e97
Jaison E Mathew <jaisonemathew14@gmail.com>
2023-07-22 22:38:27 +0530

    updates to day 34 and added day 35

--- Day 34 Quizz App with API/data.py
+++ Day 34 Quizz App with API/data.py
@@ -1,11 +1,6 @@
 import requests
 
-parameters = {
-    "amount": 10,
-    "type": "boolean",
-}
-
-response = requests.get("https://opentdb.com/api.php", params=parameters)
+response = requests.get(url="https://opentdb.com/api.php?amount=10&category=18&type=boolean")
 response.raise_for_status()
 data = response.json()
 question_data = data["results"]
--- Day 34 Quizz App with API/quiz_brain.py
+++ Day 34 Quizz App with API/quiz_brain.py
@@ -1,6 +1,4 @@
 import html
-
-
 class QuizBrain:
 
     def __init__(self, q_list):
