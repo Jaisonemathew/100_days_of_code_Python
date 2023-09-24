319d1f34dbb46a73f3d97d2701654d91a11f69a3
Jaison E Mathew <jaisonemathew14@gmail.com>
2023-09-24 22:12:43 +0530

    added day 55

---
+++ Day 55 HTML URL parsing in Flask/main.py
@@ -0,0 +1,27 @@
+from flask import Flask
+import random
+app = Flask(__name__)
+number = random.randint(0,9)
+
+@app.route('/')
+def index():
+    return "<h1>Guess a number between 0 and 9</h1>"\
+            "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif' width=200>"
+            
+
+@app.route("/<int:guess>")
+def display(guess):
+    if guess==number:
+        return f"<h1>You guessed {guess}</h1>"\
+            "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif' width=200>"
+    elif guess>number:
+        return f"<h1>You guessed {guess}</h1>"\
+            "<h1>Too high, try again!</h1>"\
+            "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif' width=200>"
+    else:
+        return f"<h1>You guessed {guess}</h1>"\
+            "<h1>Too low, try again!</h1>"\
+            "<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif' width=200>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
