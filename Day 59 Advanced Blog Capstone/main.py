906c9bea262bea8348fbc5c7187c550229f61600
Jaison E Mathew <jaisonemathew14@gmail.com>
2023-11-27 15:35:26 +0530

    completed day 59

--- Day 59 Advanced Blog Capstone/main.py
+++ Day 59 Advanced Blog Capstone/main.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+import requests
+
+posts=requests.get("https://api.npoint.io/a059da1db75e77b2f515").json()
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html",all_posts=posts)
+
+@app.route('/post/<int:p_id>')
+def get_post(p_id):
+    return render_template('post.html', post=posts[int(p_id) - 1])
 
 @app.route("/about")
 def about():
